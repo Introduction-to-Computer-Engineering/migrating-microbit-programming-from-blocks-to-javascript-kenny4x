624f014d1ebe2d73f201fe8d29d399644a3691dd
kenny4x <54786780+kenny4x@users.noreply.github.com>
2019-09-29 22:24:25 -0700

    Commit section 9

---
+++ section9.js
@@ -0,0 +1,19 @@
+// This is the code for section 9
+
+// input.onButtonPressed(Button.A, function () {
+    binary = "" + binary + "1"
+    basic.showString(binary)
+    decimal = decimal * 2 / 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(decimal)
+})
+input.onButtonPressed(Button.B, function () {
+    binary = "" + binary + "0"
+    basic.showString(binary)
+    decimal = decimal * 2
+})
+let binary = ""
+let decimal = 0
+decimal = 0
+binary = ""
