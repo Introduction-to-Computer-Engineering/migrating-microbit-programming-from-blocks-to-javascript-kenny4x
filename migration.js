8409589d6ec2abc31257da20b78cc142b464f02f
kenny4x <54786780+kenny4x@users.noreply.github.com>
2019-09-29 20:20:48 -0700

    Create migration.js

---
+++ migration.js
@@ -0,0 +1,8 @@
+// This is the javascript code for section 1
+
+// basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(5000)
+})
